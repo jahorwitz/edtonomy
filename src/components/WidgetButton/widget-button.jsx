5f4793a9c391ec87b1ee1dfd008ace0d914e85b5
khuephamy <khuephumy19@gmail.com>
2023-06-20 11:57:56 -0500

    feat: widget-button

---
+++ src/components/WidgetButton/widget-button.jsx
@@ -0,0 +1,7 @@
+const widgetButton = ({ label }) => {
+  return (
+    <button className="text-bold bg-pink-300 w-[30px] h-[10px]">{label}</button>
+  );
+};
+
+export default widgetButton;
---
+++ src/components/WidgetButton/widget-button.stories.jsx
@@ -0,0 +1,8 @@
+import widgetButton from "./widget-button";
+
+export default {
+  title: "WidgetButton",
+  component: widgetButton,
+};
+
+export const Supoort = () => <widgetButton label="Support" />;
