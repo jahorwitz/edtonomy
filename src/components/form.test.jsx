abd5c90917e11c3fe6eecf3eb6aa592c8ff93d36
christopher Abreu <95243610+ChrisAbreu01@users.noreply.github.com>
2023-06-23 19:00:01 -0400

    fix: fixing unit test

--- src/components/form.test.jsx
+++ src/components/form.test.jsx
@@ -1,8 +1,16 @@
 import {render, screen, cleanup} from '@testing-library/react'
 import FormContainer from './form-container'
-
+import Form from './form'
+const children = (
+    <>
+  <Form.TextArea labelText={"What were you trying to do?"} placeHolder={"I was trying to create a new teaching event..."}/>
+  <Form.TextArea labelText={"What were you trying to do?"} placeHolder={"I was trying to create a new teaching event..."}/>
+  <Form.TextArea labelText={"What were you trying to do?"} placeHolder={"I was trying to create a new teaching event..."}/>
+  <Form.TextArea labelText={"Is this Ok?"} placeHolder={"Yes It is "}/>
+  </>
+  )
 test("render test from component", () => {
-    render(<FormContainer/>);
+    render(<FormContainer {...children}/>);
     const formElement = screen.getByTestId("form-1");
     expect(formElement).toBeInTheDocument();
 })
