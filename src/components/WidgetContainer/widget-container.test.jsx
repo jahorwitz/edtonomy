be1ec2a5fa40fba355e758e7d786c3b7e4d23ca4
khuephamy <khuephumy19@gmail.com>
2023-06-27 15:31:52 -0500

    fix: some update

--- src/components/WidgetContainer/widget-container.test.jsx
+++ src/components/WidgetContainer/widget-container.test.jsx
@@ -2,5 +2,5 @@ import { render } from "@testing-library/react";
 import WidgetContainer from "./widget-container";
 
 test("renders WidgetContainer component", () => {
-  render(<WidgetContainer/>)
-})
+  render(<WidgetContainer />);
+});
