e5ab1f85113f919b1191476341f13af03e07798c
christopher Abreu <95243610+ChrisAbreu01@users.noreply.github.com>
2023-06-28 18:05:27 -0400

    Feature/re usable form (#2)
    * feat: From.TextArea created  react-hook-form was added
    
    * feat: form layout is complete
    
    * feat: setting up stories
    
    * feat: Unit testing created and ready, inter font and other stiles were added
    
    * feat: styles fixed
    
    * feat: storie complete corrections done
    
    * fix: setting the correct name for textarea in the Register function from react-hook-form
    
    * fix: fixing unit test
    
    * docs: comments added
    
    * style: prettier formating
    
    * style: prettier formating
    
    * refactor: adding export to index.js
    
    * Apply suggestions from code review
    
    Co-authored-by: Joshua Horwitz <jahorwitz94@gmail.com>
    
    * 1: form-container.jsx deleted 2:form-container.stories.jsx was rename to form.stories.jsx
    
    * style: prettier formating
    
    * style: prettier formating
    
    * style: prettier formating
    
    * style: prettier formating
    
    ---------
    
    Co-authored-by: Joshua Horwitz <jahorwitz94@gmail.com>

---
+++ src/components/form.jsx
@@ -0,0 +1,40 @@
+import cx from "classnames";
+import { useForm } from "react-hook-form";
+// the Form component take as argument the children elements, an onSubmit function and atributes for a form tag.
+export const Form = ({ className, children, onSubmit, ...rest }) => {
+  const {
+    handleSubmit,
+    // formState: { errors },
+  } = useForm();
+  return (
+    // form element
+    <form className={className} onSubmit={handleSubmit(onSubmit)} {...rest}>
+      {children}
+    </form>
+  );
+};
+// the Form.TextArea element take as argument props and htmlInputElement
+Form.TextArea = ({ className, labelText, id, ...rest }) => {
+  // the register function from recat-hook-form is been call here
+  const { register } = useForm();
+  return (
+    <div className={cx("flex flex-col", className)}>
+      <label
+        htmlFor={id}
+        className="font-medium text-[1.25rem] text-black leading-[120%]"
+      >
+        {labelText}
+      </label>
+      <textarea
+        id={id}
+        className="max-w-[363px] h-[105px] text-black font-medium leading-[120%] text-[1rem] pl-[12px] pt-[12px] pr-[12px] resize-none mt-[16px] mb-[24px] border border-solid border-black/[0.4] rounded-lg "
+        type="text"
+        {...rest}
+        //registering the name for the text are in the hook
+        {...register("Form-TextArea")}
+      />
+    </div>
+  );
+};
+
+Form.TextArea.displayName = "Form.TextArea";
