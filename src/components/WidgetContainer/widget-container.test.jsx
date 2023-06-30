6ae8c994d5fab4186707631d39e8f0018cd63226
khuephamy <khuephumy19@gmail.com>
2023-06-30 18:05:36 -0500

    fix: delete unused code

--- src/components/WidgetContainer/widget-container.test.jsx
+++ src/components/WidgetContainer/widget-container.test.jsx
@@ -1,15 +1,3 @@
-// import { fireEvent, render, screen } from "@testing-library/react";
-// import WidgetContainer from "./widget-container";
-
-// test("renders WidgetContainer component", () => {
-//   render(<WidgetContainer />);
-// });
-
-// test("clicking the button opens the container", () => {
-//   fireEvent.click(screen.getByLabelText("Support"));
-//   expect(screen.getByText("Welcom, Carlo!")).toBeInTheDocument();
-// });
-
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import WidgetContainer from "./widget-container";
 
@@ -20,7 +8,6 @@ test("clicking the support button shows the widget", async () => {
   expect(screen.queryByText("Welcom, Carlo!")).not.toBeInTheDocument();
 
   // Simulate a click on the support button
-  // fireEvent.click(screen.getByLabelText("Support"));
   fireEvent.click(screen.getByText("Support"));
 
   // Wait for the animation to complete and the widget to be visible
