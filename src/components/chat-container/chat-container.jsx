d422a6cbfc97d4e257aeb30400c28323a5ca99e8
christopher Abreu <95243610+ChrisAbreu01@users.noreply.github.com>
2023-07-04 20:21:09 -0400

    style: prettier formating

--- src/components/chat-container/chat-container.jsx
+++ src/components/chat-container/chat-container.jsx
@@ -1,12 +1,17 @@
-
-const ChatContainer = ({children, onChatNow, onConversations}) => {
+const ChatContainer = ({ children, onChatNow, onConversations }) => {
   return (
     <>
       <div className="w-[371px] h-[583px] mr-[auto] ml-[auto] bg-[#ED6647] rounded-[12px] flex flex-col">
-        <button className="flex flex-row text-white ml-[25px] mr-[45px] mt-[35px] mb-[35px]" onClick={onConversations}>
+        <button
+          className="flex flex-row text-white ml-[25px] mr-[45px] mt-[35px] mb-[35px]"
+          onClick={onConversations}
+        >
           <div className="flex flex-row relative mr-6">
             <img src="/src/images/chat-left-text.svg" />
-            <img className="absolute left-2 top-2" src="/src/images/chat-right.svg" />
+            <img
+              className="absolute left-2 top-2"
+              src="/src/images/chat-right.svg"
+            />
           </div>
           Conversations
         </button>
@@ -16,7 +21,7 @@ const ChatContainer = ({children, onChatNow, onConversations}) => {
             <img src="/src/images/chat-left-text.svg" />
             Chat now
           </button>
-        </div> 
+        </div>
       </div>
     </>
   );
--- src/components/chat-container/chat-container.stories.jsx
+++ src/components/chat-container/chat-container.stories.jsx
@@ -1,14 +1,12 @@
-import ChatContainer from "./chat-container"; 
+import ChatContainer from "./chat-container";
 export default {
   component: ChatContainer,
   title: "chat container",
 };
 const Template = (args) => {
-  return <ChatContainer {...args}/>;
+  return <ChatContainer {...args} />;
 };
 export const Default = Template.bind({});
 Default.args = {
-  children: [
-    
-  ],
+  children: [],
 };
