10fe7eba300e3297d2d806d52c1e756b220f60ac
christopher Abreu <95243610+ChrisAbreu01@users.noreply.github.com>
2023-07-05 15:40:37 -0400

    feat: chat-container UI is complete

--- src/components/chat-container/chat-container.jsx
+++ src/components/chat-container/chat-container.jsx
@@ -1,24 +1,36 @@
-const ChatContainer = ({ children, onChatNow, onConversations }) => {
+const ChatContainer = ({
+  children,
+  onChatNow,
+  onConversations,
+  isChatActive,
+}) => {
   return (
     <>
-      <div className="w-[371px] h-[583px] mr-[auto] ml-[auto] bg-[#ED6647] rounded-[12px] flex flex-col">
+      <div className="w-[371px] h-[583px] mr-[auto] ml-[auto] bg-[#ED6647] rounded-[12px] flex flex-col font-['Inter']">
         <button
-          className="flex flex-row text-white ml-[25px] mr-[45px] mt-[35px] mb-[35px]"
+          className="flex flex-row text-white items-center ml-[45px] mr-[45px] mt-[35px] mb-[35px] w-[300px] text-2xl"
           onClick={onConversations}
         >
-          <div className="flex flex-row relative mr-6">
-            <img src="/src/images/chat-left-text.svg" />
+          <div className="flex flex-row relative mr-6  bg-[#CC5B40] w-[50px] h-[50px] rounded-[40px]">
             <img
-              className="absolute left-2 top-2"
+              className="w-[20px] h-[20px] mt-3 ml-3"
+              src="/src/images/chat-left-text.svg"
+            />
+            <img
+              className="absolute left-4 top-5 w-[20px] h-[20px]"
               src="/src/images/chat-right.svg"
             />
           </div>
           Conversations
         </button>
-        <div>
-          <div>{children}</div>
-          <button onClick={onChatNow}>
-            <img src="/src/images/chat-left-text.svg" />
+        <div className="bg-[#FFFFFF] h-[100%] rounded-[12px] ">
+          {isChatActive ? <div>{children}</div> : <div className="flex flex-col items-center h-[300px] "><h2 className="mt-[40px] mb-[40px] font-bold text-lg">This is embedded interface</h2><img className="w-[200px] mb-[40px] opacity-[0.3]" src="/src/images/pixel-speech-bubble.png"/><p className="opacity-[0.3]">No ongoing conversation</p></div>}
+
+          <button
+            className="bg-[#ED6647] flex flex-row items-center ml-[auto] mr-[auto] rounded-[5px] w-[125px] h-[35px] justify-center text-white"
+            onClick={onChatNow}
+          >
+            <img className="mr-[10px]" src="/src/images/chat-left-text.svg" />
             Chat now
           </button>
         </div>
