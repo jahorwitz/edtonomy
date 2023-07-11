c650ac3bfa39c5affa47c019010901361a8e3589
LavisCannon15 <lantigua196@gmail.com>
2023-07-11 19:50:06 -0400

    style: changing how modal opens and closes

--- src/components/NpsFormModal/NpsFormModal.jsx
+++ src/components/NpsFormModal/NpsFormModal.jsx
@@ -86,147 +86,151 @@ export default function NpsFormModal() {
     setIsModalOpen(false);
   };
 
+  /*
   if (!isModalOpen) {
     return null; // Return null if the modal should be closed
   }
+  */
 
   const [selectedRating, setSelectedRating] = useState(0);
 
   return (
-    <div className="z-0 bg-black bg-opacity-50 w-full h-882 flex-shrink-0 relative">
-      <form
-        className="w-420 h-auto flex-shrink-0 rounded-12 bg-white relative top-243 bottom-243 left-510 z-1"
-        onSubmit={handleFormSubmit}
-      >
-        <button
-          className="absolute bg-exitbutton left-440  w-24 h-24 flex-shrink-0 ]"
-          onClick={handleModalClose}
-        ></button>
-        {isFormSubmitted ? (
-          <div className="pl-6">
-            <h1 className="text-black text-[20px] font-inter font-medium leading-120 pt-8">
-              Thanks for your feedback!
-            </h1>
-          </div>
-        ) : (
-          <div className="pl-6">
-            <h1 className="text-black text-[20px] font-inter font-medium leading-120 pt-8">
-              Hey, do you enjoy using EdTonomy?
-            </h1>
-            <div className="w-372 h-54 flex-shrink-0 flex flex-row gap-x-2 pt-4">
-              <Form
-                onSubmit={handleSubmit((values) => {
-                  alert(JSON.stringify(values));
-                })}
-                onClick={() => setNpsButtonClicked(true)}
-              >
-                <Form.RadioButtonGroup
-                  id="nps-score"
-                  className="flex gap-2 my-3 "
-                  setValue={setValue}
-                  watch={watch}
-                  options={[
-                    {
-                      value: 1,
-                      Icon: Awful,
-                      text: "Awful",
-                    },
-                    {
-                      value: 2,
-                      Icon: So_so,
-                      text: "So-so",
-                    },
-                    {
-                      value: 3,
-                      Icon: Okay,
-                      text: "Okay",
-                    },
-                    {
-                      value: 4,
-                      Icon: Good,
-                      text: "Good",
-                    },
-                    {
-                      value: 5,
-                      Icon: Great,
-                      text: "Great",
-                    },
-                  ]}
-                />
-              </Form>
+    isModalOpen && (
+      <div className="z-0 bg-black bg-opacity-50 w-full h-882 flex-shrink-0 relative">
+        <form
+          className="w-420 h-auto flex-shrink-0 rounded-12 bg-white relative top-243 bottom-243 left-510 z-1"
+          onSubmit={handleFormSubmit}
+        >
+          <button
+            className="absolute bg-exitbutton left-440  w-24 h-24 flex-shrink-0 ]"
+            onClick={handleModalClose}
+          ></button>
+          {isFormSubmitted ? (
+            <div className="pl-6">
+              <h1 className="text-black text-[20px] font-inter font-medium leading-120 pt-8">
+                Thanks for your feedback!
+              </h1>
             </div>
+          ) : (
+            <div className="pl-6">
+              <h1 className="text-black text-[20px] font-inter font-medium leading-120 pt-8">
+                Hey, do you enjoy using EdTonomy?
+              </h1>
+              <div className="w-372 h-54 flex-shrink-0 flex flex-row gap-x-2 pt-4">
+                <Form
+                  onSubmit={handleSubmit((values) => {
+                    alert(JSON.stringify(values));
+                  })}
+                  onClick={() => setNpsButtonClicked(true)}
+                >
+                  <Form.RadioButtonGroup
+                    id="nps-score"
+                    className="flex gap-2 my-3 "
+                    setValue={setValue}
+                    watch={watch}
+                    options={[
+                      {
+                        value: 1,
+                        Icon: Awful,
+                        text: "Awful",
+                      },
+                      {
+                        value: 2,
+                        Icon: So_so,
+                        text: "So-so",
+                      },
+                      {
+                        value: 3,
+                        Icon: Okay,
+                        text: "Okay",
+                      },
+                      {
+                        value: 4,
+                        Icon: Good,
+                        text: "Good",
+                      },
+                      {
+                        value: 5,
+                        Icon: Great,
+                        text: "Great",
+                      },
+                    ]}
+                  />
+                </Form>
+              </div>
 
-            <h1 className="text-black text-[20px] font-inter font-medium leading-120 pt-8 mt-8">
-              Would you recommend EdTonomy to a friend?
-            </h1>
+              <h1 className="text-black text-[20px] font-inter font-medium leading-120 pt-8 mt-8">
+                Would you recommend EdTonomy to a friend?
+              </h1>
 
-            <div className="flex flex-row gap-x-5 pt-4">
-              <RadioButton
-                onClick={handleYesButtonClick}
-                selected={feedbackSubmittedYes}
-                text="Yes"
-                className="w-44"
-              />
-              <RadioButton
-                onClick={handleNoButtonClick}
-                selected={feedbackSubmittedNo}
-                text="No"
-                className="w-44"
-              />
-            </div>
-
-            {feedbackSubmittedYes && (
-              <>
-                <p className="text-black text-center text-[18px] font-inter leading-120 flex items-center justify-center flex-wrap flex-shrink-0 mt-5 w-372">
-                  Read more about our
-                  <span className="text-red-700 underline ml-1">
-                    referral program
-                  </span>
-                </p>
-              </>
-            )}
-            {feedbackSubmittedNo && (
-              <>
-                <h1 className="text-black text-[20px] font-inter font-medium leading-120 pt-8">
-                  What stops you from recommending EdTonomy to a friend?
-                </h1>
-
-                <textarea
-                  className={`w-372 h-105 rounded-lg border border-black-40 bg-white mt-4 pt-2.5 pl-2.5 ${
-                    isFeedbackValid ? "" : "invalid"
-                  }`}
-                  type="text"
-                  required
-                  value={feedbackText}
-                  onChange={handleFeedbackInputChange}
-                  placeholder="Tell us"
+              <div className="flex flex-row gap-x-5 pt-4">
+                <RadioButton
+                  onClick={handleYesButtonClick}
+                  selected={feedbackSubmittedYes}
+                  text="Yes"
+                  className="w-44"
                 />
-              </>
-            )}
-            <Button
-              onClick={handleFormSubmit}
-              className={`w-372 h-54 flex-shrink-0 rounded-lg bg-edt-red  mt-8 text-white text-center text-[20px] font-medium font-inter leading-120 ${
-                (feedbackSubmittedYes || feedbackSubmittedNo) &&
-                isFeedbackValid &&
-                npsButtonClicked
-                  ? "opacity-100"
-                  : "opacity-30"
-              }`}
-              type="submit"
-              disabled={
-                !(
+                <RadioButton
+                  onClick={handleNoButtonClick}
+                  selected={feedbackSubmittedNo}
+                  text="No"
+                  className="w-44"
+                />
+              </div>
+
+              {feedbackSubmittedYes && (
+                <>
+                  <p className="text-black text-center text-[18px] font-inter leading-120 flex items-center justify-center flex-wrap flex-shrink-0 mt-5 w-372">
+                    Read more about our
+                    <span className="text-red-700 underline ml-1">
+                      referral program
+                    </span>
+                  </p>
+                </>
+              )}
+              {feedbackSubmittedNo && (
+                <>
+                  <h1 className="text-black text-[20px] font-inter font-medium leading-120 pt-8">
+                    What stops you from recommending EdTonomy to a friend?
+                  </h1>
+
+                  <textarea
+                    className={`w-372 h-105 rounded-lg border border-black-40 bg-white mt-4 pt-2.5 pl-2.5 ${
+                      isFeedbackValid ? "" : "invalid"
+                    }`}
+                    type="text"
+                    required
+                    value={feedbackText}
+                    onChange={handleFeedbackInputChange}
+                    placeholder="Tell us"
+                  />
+                </>
+              )}
+              <Button
+                onClick={handleFormSubmit}
+                className={`w-372 h-54 flex-shrink-0 rounded-lg bg-edt-red  mt-8 text-white text-center text-[20px] font-medium font-inter leading-120 ${
                   (feedbackSubmittedYes || feedbackSubmittedNo) &&
                   isFeedbackValid &&
                   npsButtonClicked
-                )
-              }
-            >
-              Send feedback
-            </Button>
-          </div>
-        )}
-        ;
-      </form>
-    </div>
+                    ? "opacity-100"
+                    : "opacity-30"
+                }`}
+                type="submit"
+                disabled={
+                  !(
+                    (feedbackSubmittedYes || feedbackSubmittedNo) &&
+                    isFeedbackValid &&
+                    npsButtonClicked
+                  )
+                }
+              >
+                Send feedback
+              </Button>
+            </div>
+          )}
+          ;
+        </form>
+      </div>
+    )
   );
 }
