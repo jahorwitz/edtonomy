bbb8e51dd4cd654c4341112fcfd7e3cf3e8bb41e
LavisCannon15 <lantigua196@gmail.com>
2023-07-12 21:26:28 -0400

    style: replaced inputs with button and form

--- src/components/Article/Questionnaire.jsx
+++ src/components/Article/Questionnaire.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
 
+import { Form } from "../../components/form/form";
 import { SecondaryOutlineButton } from "../button/button";
 
 export default function Questionnaire() {
   const [feedbackSubmittedYes, setFeedbackSubmittedYes] = useState(false);
   const [feedbackSubmittedNo, setFeedbackSubmittedNo] = useState(false);
   const [formSubmit, setFormSubmit] = useState(false);
-
-  const [feedbackText, setFeedbackText] = useState("");
 
   const handleYesButtonClick = () => {
     setFeedbackSubmittedYes(true);
@@ -16,11 +15,9 @@ export default function Questionnaire() {
   const handleNoButtonClick = () => {
     setFeedbackSubmittedNo(true);
     setFeedbackSubmittedYes(false);
-    setFeedbackText("");
   };
 
   const handleFormSubmit = (e) => {
-    e.preventDefault();
     // Perform the necessary actions with the feedback text, such as sending it to a server
     // You can access the feedback text using the feedbackText state variable
     setFormSubmit(true);
@@ -29,14 +26,14 @@ export default function Questionnaire() {
   return (
     <div className="mt-16">
       {feedbackSubmittedYes ? (
-        <h1 className="text-black text-2xl font-inter font-semibold leading-130%">
+        <h1 className="font-medium text-[1.25rem] text-black leading-[120%]">
           We're glad it was useful
         </h1>
       ) : (
         <>
           {!feedbackSubmittedNo && (
             <>
-              <h1 className="text-black text-2xl font-inter font-semibold leading-130%">
+              <h1 className="font-medium text-[1.25rem] text-black leading-[120%]">
                 Was this article helpful?
               </h1>
               <div className="flex flex-row gap-5 mt-5">
@@ -53,25 +50,20 @@ export default function Questionnaire() {
           )}
           {feedbackSubmittedNo && !formSubmit && (
             <>
-              <h1 className="text-black text-2xl font-inter font-semibold leading-130%">
-                What was unclear?
-              </h1>
-              <form className="flex flex-col mt-5" onSubmit={handleFormSubmit}>
-                <input
-                  className="w-363 h-105 rounded-md border border-black-40 bg-white mb-5"
-                  type="text"
-                  value={feedbackText}
-                  onChange={(e) => setFeedbackText(e.target.value)}
+              <Form className="flex flex-col mt-5" onSubmit={handleFormSubmit}>
+                <Form.TextArea
+                  labelText="What was unclear?"
+                  id="feedbackTextArea"
                   placeholder="Tell us"
                   required
                 />
 
                 <SecondaryOutlineButton text="Submit" type="submit" />
-              </form>
+              </Form>
             </>
           )}
           {formSubmit && (
-            <h1 className="text-black text-2xl font-inter font-semibold leading-130%">
+            <h1 className="font-medium text-[1.25rem] text-black leading-[120%]">
               Thanks for your feedback
             </h1>
           )}
